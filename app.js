b655acc4518dae53a170291d185b6ed1097dab26
AlanSquid <squid78313@gmail.com>
2022-09-29 01:49:27 +0800

    Random password generator useful

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const generatePassword = require('./generate_password.js')
 
 
 const exphbs = require('express-handlebars')
@@ -14,8 +15,8 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log('req.body', req.body)
-  res.render('index')
+  const password = generatePassword(req.body)
+  res.render('index', { password })
 })
 
 app.listen(port, () => {
