66da482e32638e17ab7cd036f9c8f5eada176e26
AlanSquid <squid78313@gmail.com>
2022-09-29 00:29:52 +0800

    Use req.body to get form-post

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log('req.body', req.body)
   res.render('index')
 })
 
